cf93bc6eea7d20064afd609d6871371a31ba44f2
hieuntp2 <hieuntp2@gmail.com>
2015-03-24 20:17:20 +0700

    first learn

---
+++ dota2chathub/Module/PublicChat/PublicChatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dota2chathub.Module.PublicChat
+{
+    public class PublicChatController : Controller
+    {
+        // GET: PublicChat
+        public ActionResult Index()
+        {
+            return PartialView("~/Module/PublicChat/BoxPublicChat.cshtml");
+        }
+    }
+}
---
+++ dota2chathub/Scripts/homeindexscript.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module('main_app', []);
+
+app.controller('modulecontroller', ['$http', '$scope', '$compile', function ($http, $scope, $compile) {
+    var ctrll = this;
+    this.modules = [];
+
+    this.getmodule = function (address, id_div) {
+        $http.get(address).success(function (data) {
+
+            var el = angular.element(data),
+            compiled = $compile(el);
+
+            $("#" + id_div + "").append(el);
+
+
+        }).error(function () {
+            alert("Lỗi khi lấy module " + address);
+        });
+    }
+
+    this.init = function () {
+        ctrll.getmodule('../../PublicChat/Index', 'main_col_6');
+    }
+}])
+
+
